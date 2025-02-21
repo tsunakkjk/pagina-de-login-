2edec148bca94a8be15cf42d914b55ec8cffade8
Tsuna <joaolokis6@gmail.com>
2025-02-21 16:45:22 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get("/home", (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+app.listen(port, ()=>{
+    console.log("http://localhost:3000");
+});
